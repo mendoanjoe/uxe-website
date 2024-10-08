4382184c9c36cbfbae1f6b7d8900e2541b72ee87
Firmansyah Nuralif Rohman <mendoanjoe@gmail.com>
2024-10-08 21:33:33 +0700

    navigation

--- stories/ui/base/menu-item/MenuItem.tsx
+++ stories/ui/base/menu-item/MenuItem.tsx
@@ -19,8 +19,10 @@ export const MenuItem = ({ menu, isFixed }: MenuItemProps) => {
     "hover:text-[#19191B] hover:bg-[#BEBEBE1F]": isFixed,
   });
 
+  console.log(menu)
+
   return (
-    menu.map(({ name, url, submenu }, index) => (
+    menu.map(({ name, url, submenu, level }, index) => (
       <li key={index} className='group cursor-pointer max-md:border-b-[.5px] max-md:border-b-[#19191b1a] relative' id={`menu-item-${index}`}>
         {submenu.length > 0 ? (
           <div className={containerClass}>
@@ -32,36 +34,71 @@ export const MenuItem = ({ menu, isFixed }: MenuItemProps) => {
                 </svg>
               </div>
               <div className='max-md:mt-[14px]' id={`submenu-item-${index}`}>
-                <div className='hidden absolute overflow-hidden left-[50%] translate-x-[-50%] top-[105%] bg-white text-black z-50 rounded-[16px] group-hover:p-3 group-hover:h-auto group-hover:flex shadow-md gap-5'>
+                <div className={`hidden absolute overflow-hidden left-[50%] translate-x-[-50%] top-[105%] bg-white text-black z-50 rounded-[16px] group-hover:p-3 group-hover:h-auto shadow-md gap-5 ${level == 4 ? "group-hover:flex" : "group-hover:grid"}`}>
                   {submenu.map((item, idn) => (
                     <div key={idn}>
                       {item.name == undefined ? (
                         <div>
                           <TextXSmall key={idn} label={item.group} cls='font-bold uppercase mb-5' />
-                          <div className={idn == 0 ? `flex gap-6 flex-col` : `grid grid-cols-2`}>
+                          <div className={idn == 0 ? `flex gap-6 flex-col` : `grid grid-cols-2 gap-5`}>
                             {item.submenu.map((item, idd) => (
-                              <a key={idd} href={item.url} className='group/submenu flex gap-[4px] items-center justify-between hover:bg-[#BEBEBE1F] p-[12px] rounded-[8px] min-w-44'>
-                                <div className='flex flex-col gap-[6px]'>
-                                  <h3 className='text-[#19191B] text-[16px] font-normal leading-[132%] -tracking-[.16px]'>{item.name}</h3>
-                                  {/* <p className='text-[#19191B80] text-[12px] font-[400] leading-[132%] -tracking-[.12px]'>{description}</p> */}
-                                </div>
-                                <div className='size-[20px] hidden group-hover/submenu:block'>
-                                  <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <g filter="url(#filter0_b_440_8236)">
-                                    <rect y="0.5" width="20" height="20" rx="10" fill="#19191B"/>
-                                    <path d="M12.458 8.38L12.468 12.79L11.708 13.55L11.688 9.74L7.72797 13.69L7.15797 13.12L11.138 9.14H7.30797L8.06797 8.38H12.458Z" fill="white"/>
-                                    </g>
-                                    <defs>
-                                    <filter id="filter0_b_440_8236" x="-1.66667" y="-1.16667" width="23.3333" height="23.3333" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                                    <feFlood floodOpacity="0" result="BackgroundImageFix"/>
-                                    <feGaussianBlur in="BackgroundImageFix" stdDeviation="0.833333"/>
-                                    <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_440_8236"/>
-                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_440_8236" result="shape"/>
-                                    </filter>
-                                    </defs>
-                                  </svg>
-                                </div>
-                              </a>
+                              <div>
+                                {item.name == undefined ? (
+                                  <div className='min-w-48'>
+                                    <TextMedium key={idd} label={item.group} cls='font-bold' />
+                                    <div className='my-2 border-b border-[#19191B17]'></div>
+                                    <div className='flex flex-col gap-[6px]'>
+                                    {item.submenu.map((item, lol) => (
+                                      <a key={lol} href={item.url} className='group/submenu flex gap-[4px] items-center justify-between hover:bg-[#BEBEBE1F] py-[6px] px-2 rounded-[8px]'>
+                                        <div className='flex flex-col gap-[6px]'>
+                                          <h3 className='text-[#19191B] text-[16px] font-normal leading-[132%] -tracking-[.16px]'>{item.name}</h3>
+                                          {/* <p className='text-[#19191B80] text-[12px] font-[400] leading-[132%] -tracking-[.12px]'>{description}</p> */}
+                                        </div>
+                                        <div className='size-[20px] hidden group-hover/submenu:block'>
+                                          <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                            <g filter="url(#filter0_b_440_8236)">
+                                            <rect y="0.5" width="20" height="20" rx="10" fill="#19191B"/>
+                                            <path d="M12.458 8.38L12.468 12.79L11.708 13.55L11.688 9.74L7.72797 13.69L7.15797 13.12L11.138 9.14H7.30797L8.06797 8.38H12.458Z" fill="white"/>
+                                            </g>
+                                            <defs>
+                                            <filter id="filter0_b_440_8236" x="-1.66667" y="-1.16667" width="23.3333" height="23.3333" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                                            <feGaussianBlur in="BackgroundImageFix" stdDeviation="0.833333"/>
+                                            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_440_8236"/>
+                                            <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_440_8236" result="shape"/>
+                                            </filter>
+                                            </defs>
+                                          </svg>
+                                        </div>
+                                      </a>
+                                    ))}
+                                    </div>
+                                  </div>
+                                ) : (
+                                  <a key={idd} href={item.url} className='group/submenu flex gap-[4px] items-center justify-between hover:bg-[#BEBEBE1F] p-[12px] rounded-[8px] min-w-44'>
+                                    <div className='flex flex-col gap-[6px]'>
+                                      <h3 className='text-[#19191B] text-[16px] font-normal leading-[132%] -tracking-[.16px]'>{item.name}</h3>
+                                      {/* <p className='text-[#19191B80] text-[12px] font-[400] leading-[132%] -tracking-[.12px]'>{description}</p> */}
+                                    </div>
+                                    <div className='size-[20px] hidden group-hover/submenu:block'>
+                                      <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g filter="url(#filter0_b_440_8236)">
+                                        <rect y="0.5" width="20" height="20" rx="10" fill="#19191B"/>
+                                        <path d="M12.458 8.38L12.468 12.79L11.708 13.55L11.688 9.74L7.72797 13.69L7.15797 13.12L11.138 9.14H7.30797L8.06797 8.38H12.458Z" fill="white"/>
+                                        </g>
+                                        <defs>
+                                        <filter id="filter0_b_440_8236" x="-1.66667" y="-1.16667" width="23.3333" height="23.3333" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                        <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                                        <feGaussianBlur in="BackgroundImageFix" stdDeviation="0.833333"/>
+                                        <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_440_8236"/>
+                                        <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_440_8236" result="shape"/>
+                                        </filter>
+                                        </defs>
+                                      </svg>
+                                    </div>
+                                  </a>
+                                )}
+                              </div>
                             ))}
                           </div>
                         </div>
@@ -78,8 +115,8 @@ export const MenuItem = ({ menu, isFixed }: MenuItemProps) => {
                               <path d="M12.458 8.38L12.468 12.79L11.708 13.55L11.688 9.74L7.72797 13.69L7.15797 13.12L11.138 9.14H7.30797L8.06797 8.38H12.458Z" fill="white"/>
                               </g>
                               <defs>
-                              <filter id="filter0_b_440_8236" x="-1.66667" y="-1.16667" width="23.3333" height="23.3333" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                              <feFlood floodOpacity="0" result="BackgroundImageFix"/>
+                              <filter id="filter0_b_440_8236" x="-1.66667" y="-1.16667" width="23.3333" height="23.3333" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                              <feFlood flood-opacity="0" result="BackgroundImageFix"/>
                               <feGaussianBlur in="BackgroundImageFix" stdDeviation="0.833333"/>
                               <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_440_8236"/>
                               <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_440_8236" result="shape"/>
